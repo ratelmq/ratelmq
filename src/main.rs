40017ea1e815f747a222b9b099c3fc727f8b6d4d
Wojciech Wilk <galhadpl@gmail.com>
2020-11-08 23:06:28 +0100

    Basic listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:1883").await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
+
+async fn process(mut socket: TcpStream) {
+    let (mut rd, mut wr) = socket.split();
+    if tokio::io::copy(&mut rd, &mut wr).await.is_err() {
+        eprintln!("Failed to copy");
+    }
 }
